1f9981a59909730463cc474571adf2559ca56449
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-19 21:35:23 +0100

    Get next document from queue

---
+++ queue/index.js
@@ -0,0 +1,27 @@
+const { logConfig, logger } = require('@vtfk/logger')
+const HTTPError = require('../lib/http-error')
+const mongo = require('../lib/get-mongo')
+
+const handleQueue = async (context, req) => {
+  if (process.env.NODE_ENV !== "development") logConfig({ azure: { context } })
+
+  try {
+    const logs = await mongo()
+    logger('info', ['queue', 'get next document in queue'])
+    const documents = await logs.find({ isQueued: true }).sort({ timestamp: 1 }).limit(1).toArray()
+    if (documents.length > 0) {
+      logger('info', ['queue', 'get next document in queue', 'success', documents[0]._id])
+      logger('info', ['queue', 'get next document in queue', 'success', documents[0]._id, 'set to not in queue'])
+      await logs.updateOne({ _id: documents[0]._id }, { $set: { isQueued: false } })
+      logger('info', ['queue', 'get next document in queue', 'success', documents[0]._id, 'set to not in queue', 'success'])
+    } else {
+      logger('info', ['queue', 'get next document in queue', 'success', 'no documents in queue'])
+    }
+    return documents
+  } catch (error) {
+    logger('error', ['queue', 'error', error])
+    return new HTTPError(500, 'An unknown error occured', error).toJSON()
+  }
+}
+
+module.exports = (context, req) => handleQueue(context, req)
