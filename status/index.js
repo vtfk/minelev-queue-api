ff6db4f29ba84cc126881479569b03a6b2cdfdf6
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-19 22:13:50 +0100

    Update document with status

---
+++ status/index.js
@@ -0,0 +1,38 @@
+const { ObjectId } = require('mongodb')
+const { logConfig, logger } = require('@vtfk/logger')
+const HTTPError = require('../lib/http-error')
+const mongo = require('../lib/get-mongo')
+
+const handleStatus = async (context, req) => {
+  if (process.env.NODE_ENV !== "development") logConfig({ azure: { context } })
+
+  const { id } = req.params
+  const { data } = req.body
+
+  try {
+    if (id === undefined) {
+      throw new HTTPError(400, 'Missing id', null)
+    }
+
+    if (data === undefined) {
+      throw new HTTPError(400, 'Missing data', null)
+    }
+
+    const _id = ObjectId(id)
+    const logs = await mongo()
+    const status = {
+      status: data.status,
+      timeStamp: new Date().getTime()
+    }
+    logger('info', ['status', 'update', 'id', id])
+    const result = await logs.updateOne({ _id }, { "$push": { "documentStatus": status } })
+    logger('info', ['status', 'update', 'id', id, 'success'])
+    return result
+  } catch (error) {
+    logger('error', ['status', 'error', error])
+    if (error instanceof HTTPError) return error.toJSON()
+    return new HTTPError(500, 'An unknown error occured', error).toJSON()
+  }
+}
+
+module.exports = (context, req) => handleStatus(context, req)
