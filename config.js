65a8d9f02d86ca81c87ede256c19b28ad73d83ac
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-20 10:54:12 +0100

    Updated to be correct in production

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   MONGODB_CONNECTION: process.env.MONGODB_CONNECTION || undefined,
   MONGODB_COLLECTION: process.env.MONGODB_COLLECTION || undefined,
   MONGODB_NAME: process.env.MONGODB_NAME || undefined,
-  JWT_SECRET: undefined || process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || false,
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || undefined,
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || undefined,
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || undefined
